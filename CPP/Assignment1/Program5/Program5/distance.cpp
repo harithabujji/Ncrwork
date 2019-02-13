de863e2e0d34a7bc7ef374b8d23ee5b9f996c3f0
harithabujji <anuhyareddy9999@gmail.com>
2019-02-13 19:36:23 +0530

    prgm 7 and 9 added

--- CPP/Assignment1/Program5/Program5/distance.cpp
+++ CPP/Assignment1/Program5/Program5/distance.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-class Distance2;
+class Ditance2;
 class Distance1
 {
 public:
@@ -35,5 +35,21 @@ void sum(Distance1 d1, Ditance2 d2)
 	m_c = d1.mrs * 100;
 	int  ans, m, c, f, in;
 	ans = (d1.cms + m_c) + (i_c + f_c);
-
+	m = ans / 100;
+	c = ans % 100;
+	cout << "\n ------------------------------------------------------------------";
+	cout << "\n Sum in Meters & Centimeters         =  " << m << " mtrs & " << c << " cms";
+	f = m / 0.30;
+	in = c / 2.54;
+	cout << "\n Sum in Feets & Inches               =  " << f << " feets & " << in << " inches \n";
+}
+int main()
+{
+	Distance1 d1;
+	Ditance2 d2;
+	d1.get_distance();
+	d2.get_distance();
+	sum(d1, d2);
+	system("pause");
+	return 0;
 }
