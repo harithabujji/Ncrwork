eeeaaf2c2413cb8d4aebe78bf36c535aaf668e0f
harithabujji <anuhyareddy9999@gmail.com>
2019-02-15 14:18:58 +0530

    prgms added

--- CPP/Assignment1/Program9/Program9/Swap.cpp
+++ CPP/Assignment1/Program9/Program9/Swap.cpp
@@ -2,33 +2,42 @@
 using namespace std;
 class Swap
 {
-	int x, y;
     public:
-	void swap1(int x, int y)
+		int x;
+		Swap()
+		{
+			x = 0;
+		}
+		Swap(int val)
+		{
+			x = val;
+		}
+	void swap2(Swap &s1,Swap &s2)
 	{
 		int temp;
-		temp = x;
-		x = y;
-		y = temp;
-		cout << "\nswapped values: " << x << "\t" << y;
+		temp = s1.x;
+		s1.x = s2.x;
+		s2.x = temp;
+		cout << "\nswapped values: " << s1.x << "\t" << s2.x;
 	}
-	void swap2(int *x, int *y)
+	void swap1(Swap s1, Swap s2)
 	{
 		int temp;
-		temp = *x;
-		*x = *y;
-		*y = temp;
-		cout << "\nswapped value : " << *x << "\t" << *y;
+		temp = s1.x;
+		s1.x = s2.x;
+		s2.x = temp;
+		cout << "\nswapped values: " << s1.x << "\t" << s2.x;
 	}
+	
 
 };
 int main()
 {
 	int x, y, n;
-	Swap s;
 	cout << "enter two values to be swapped\n";
 	cin >> x >> y;
 	cout << "\nfor CALL BY VALUE press1";
+	Swap s, s1(x), s2(y);
 	cout << "\nfor CALL BY REFERENCE press2";
 	cin >> n;
 	switch (n)
@@ -37,15 +46,14 @@ int main()
 
 		cout << "\nCALL BY VALUE";
 		cout << "\nvalues before swap " << x << "\t" << y;
-		s.swap1(x, y);
-		cout << "\nafter swap outside of function";
-		cout << "\n x= " << x << "\n y= " << y;
+		s.swap1(s1, s2);
+		cout << "\nafter swap outside of function (valu)  " << s1.x << s2.x;
 		break;
 	case 2:
 		cout << "\nCALL BY REFERENCE";
 		cout << "\nvalue before swap  " << x << y;
-		s.swap2(&x, &y);
-		cout << "\nafter swap outside of function  " << x << y;
+		s.swap2(s1, s2);
+		cout << "\nafter swap outside of function (reference) " << s1.x << s2.x;
 		break;
 	}
 	system("pause");
