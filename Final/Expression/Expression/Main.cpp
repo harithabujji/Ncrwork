7a3075c6a2b4ebe9e6654caf1f93f649f58cb04f
harithabujji <anuhyareddy9999@gmail.com>
2019-03-07 11:24:14 +0530

    changes aadded

--- Final/Expression/Expression/Main.cpp
+++ Final/Expression/Expression/Main.cpp
@@ -2,12 +2,22 @@
 #include<iostream>
 using namespace std;
 int evaluate(string);
-
+bool areParanthesisBalanced(string);
 int main() 
 {
 	string expression;
+	cout << "Enter Expression : \n";
 	cin >> expression;
-	cout << evaluate(expression) << "\n";
+	if (areParanthesisBalanced(expression))
+	{
+		cout << "Result is : "<< evaluate(expression) << "\n";
+	}
+	else
+	{
+		cout << "\nInvalid Expression" << endl;
+		system("pause");
+		return 0;
+	}
 	system("pause");
 	return 0;
 }
--- Final/stringfun/stringfun/string.cpp
+++ Final/stringfun/stringfun/string.cpp
@@ -1,6 +1,7 @@
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX 100
 //String reverse program
 void str_reverse(char *input, char *output)
 {
@@ -31,7 +32,7 @@ void str_cat(char *input1, char *input2)
 	int length = strlen(input2);
 	int l = strlen(input1);
 	int i = 0;
-	input1 = (char*)realloc(input1, 40);
+	//input1 = (char*)realloc(input1, (length+l));
 	while (*(input2 + i) != '\0')
 	{
 		*(input1 + l) = *(input2 + i);
@@ -67,36 +68,42 @@ void str_compare(char *input1, char *input2)
 }
 int main()
 {
-	char *string1 = (char*)malloc(40);
-	char *string2 = (char*)malloc(40);
+	char *string1 = (char*)malloc(MAX);
+	char *string2 = (char*)malloc(MAX);
 	int choice = 0;
 	while (1)
 	{
-		printf("\nEnter \n 0:EXIT \n 1.Reverse \n 2.Copy \n 3.Concatnate \n 4.Comparison : \n");
+		printf("\n\nEnter \n 0:EXIT \n 1.Reverse \n 2.Copy \n 3.Concatnate \n 4.Comparison : \n");
 		scanf_s("%d", &choice);
 		switch (choice)
 		{
-		case 1:printf("Enter string to be reversed :\n");
-			scanf_s("%9s", string1, 40);
+		case 1:printf("\nEnter string to be reversed :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
 			str_reverse(string1, string2);
 			printf("Reversed string :%s ", string2);
 			break;
-		case 2:printf("Enter string to be copied :\n");
-			scanf_s("%9s", string1, 40);
+		case 2:printf("\nEnter string to be copied :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
 			str_copy(string1, string2);
-			printf("\n Copied string :%s", string2);
+			printf("Copied string :%s", string2);
 			break;
-		case 3:printf("Enter first string :\n");
-			scanf_s("%9s", string1, 40);
+		case 3:printf("\nEnter first string :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
 			printf("Enter second string :\n");
-			scanf_s("%9s", string2, 40);
+			getchar();
+			scanf_s("%[^\n]s", string2, MAX);
 			str_cat(string1, string2);
 			printf("\n Concatnated string :%s", string1);
 			break;
-		case 4:printf("Enter first string :\n");
-			scanf_s("%9s", string1, 40);
+		case 4:printf("\nEnter first string :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
 			printf("Enter second string :\n");
-			scanf_s("%9s", string2, 40);
+			getchar();
+			scanf_s("%[^\n]s", string2, MAX);
 			str_compare(string1, string2);
 			break;
 		case 0:
